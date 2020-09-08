2052eba7b82f27ca7a203d656080523f5e5955c5
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-08 18:23:40 +0200

    Fix vim config

--- home/vim.nix
+++ home/vim.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 
 {
-    programs.vim.extraConfig = ''
+    programs.vim = {
+      enable = true;
+      extraConfig = ''
         set encoding=utf8
 
-        # Fuzzy finding
+        " Fuzzy finding
         set path +=**
 
         set number
@@ -12,16 +14,16 @@
 
         syntax enable
 
-        # Show <row,col> at all times
+        " Show <row,col> at all times
         set ruler
 
         set showcmd
 
-        # Highlight matching braces
+        " Highlight matching braces
         set showmatch
 
         set incsearch
-        st hlsearch
+        set hlsearch
 
         set ignorecase
         set smartcase
@@ -31,12 +33,12 @@
         set softtabstop=4
         set expandtab
 
-        # Default to 2-space tabs for certain filetypes
+        " Default to 2-space tabs for certain filetypes
         autocmd FileType html       :setlocal sw=2 ts=2 sts=2
         autocmd FileType javascript :setlocal sw=2 ts=2 sts=2
         autocmd FileType yaml       :setlocal sw=2 ts=2 sts=2
 
-        # Make backspace sane
+        " Make backspace sane
         set backspace=eol,start,indent
 
         filetype plugin on
@@ -46,5 +48,6 @@
 
         set noerrorbells
         set novisualbell
-    '';
+      '';
+    };
 }
