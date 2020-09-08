44137006a165bf5951e37096b7e5558b0c5e3a92
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-08 18:51:13 +0200

    Add Ansible(-lint)

--- hosts/personal/configuration.nix
+++ hosts/personal/configuration.nix
@@ -73,6 +73,8 @@
 
     nixpkgs.config.allowUnfree = true;
     environment.systemPackages = with pkgs; [
+        ansible
+        ansible-lint
         bitwarden
         chromium
         dbeaver
