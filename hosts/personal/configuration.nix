84967cf99b53c5a33931718de6fc74863e8d9286
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-12 15:19:24 +0200

    Update TLP config

--- hosts/personal/configuration.nix
+++ hosts/personal/configuration.nix
@@ -65,7 +65,14 @@
 
     services = {
         openssh.enable = true;
-        tlp.enable = true;
+        tlp = {
+            enable = true;
+            extraConfig = ''
+                TLP_DEFAULT_MODE=BAT
+                CPU_SCALING_GOVERNOR_ON_AC=ondemand
+                CPU_SCALING_GOVERNOR_ON_BAT=powersave
+            '';
+        };
 
         # Suspend to ram doesn't work on the Yoga Slim 7 without some finangling.
         # This is a simple workaround.
