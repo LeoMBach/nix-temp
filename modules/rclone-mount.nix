6368964b843677aa3e165bf1b4afdcdd82576642
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-16 17:26:00 +0200

    Add rclone-mount module

--- modules/rclone-mount.nix
+++ modules/rclone-mount.nix
@@ -1,55 +1,67 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
 let
   cfg = config.services.rclone-mount;
+
+  mkRcloneMount = name: cfg:
+    nameValuePair "rclone-mount-${name}" {
+      description = "rclone mount ${name}";
+      path = with pkgs; [ rclone ];
+      preStart = "mkdir -p ${cfg.mountPath}";
+      serviceConfig = {
+        Type = "simple";
+        Environment = "PATH=$PATH:/run/current-system/sw/bin:/run/wrappers/bin/fusermount";
+        ExecStart = [''
+          /run/current-system/sw/bin/rclone mount \
+            --config=${cfg.configPath} \
+            --allow-other \
+            --cache-tmp-upload-path=/tmp/rclone/upload \
+            --cache-chunk-path=/tmp/rclone/chunks \
+            --cache-db-path=/tmp/rclone/db \
+            --cache-dir=/tmp/rclone/vfs \
+            --vfs-cache-mode=full \
+            --stats=0 \
+            ${cfg.remote} ${cfg.mountPath}
+        ''];
+        ExecStop = "/run/wrappers/bin/fusermount -u ${cfg.mountPath}";
+        Restart = "always";
+        RestartSec = "10";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+    };
 in {
-    options.services.rclone-mount = {
-        enable = mkEnableOption "rclone mount service";
-        description = mkOption {
+  options.services.rclone-mount.mounts = mkOption {
+    default = {};
+    type = types.attrsOf (types.submodule (
+      { name, ... }: {
+        options = {
+          remote = mkOption {
             type = types.str;
-            default = "";
-        };
-        remoteName = mkOption {
-            type = types.str;
-        };
-        mountDir = mkOption {
-            type = types.str;
-            description = "The target mount directory. Will be created if it does not exist.";
-            default = "/mnt/dropbox"; # TODO: Dynamic mount dir
-        };
-        configPath = mkOption {
+            description = "Remote name.";
+            example = "dropbox:";
+          };
+
+          configPath = mkOption {
             type = types.str;
             description = "Absolute path to rclone.conf.";
-        };
-    };
+            example = "/root/.config/rclone/rclone.conf";
+          };
 
-    config = {
-        systemd.services.rclone-mount = {
-            description = "${cfg.description}";
-            after = [ "network-online.target" ];
-            preStart = "mkdir -p ${cfg.mountDir}";
-            serviceConfig = {
-                Type = "simple";
-                Environment = "PATH=$PATH:/run/current-system/sw/bin:/run/wrappers/bin/fusermount";
-                ExecStart = [''
-                    /run/current-system/sw/bin/rclone mount \
-                        --config=${cfg.configPath} \
-                        --allow-other \
-                        --cache-tmp-upload-path=/tmp/rclone/upload \
-                        --cache-chunk-path=/tmp/rclone/chunks \
-                        --cache-db-path=/tmp/rclone/db \
-                        --cache-dir=/tmp/rclone/vfs \
-                        --vfs-cache-mode=full \
-                        --stats=0 \
-                        ${cfg.remoteName} ${cfg.mountDir}
-                ''];
-                ExecStop = "/run/wrappers/bin/fusermount -u ${cfg.mountDir}";
-                Restart = "always";
-                RestartSec = "10";
-            };
-            wantedBy = [ "multi-user.target" ];
+          mountPath = mkOption {
+            type = types.str;
+            description = "Absolute path to target mount directory.";
+            example = "/mnt/mymount";
+          };
         };
-    };
+      }
+    ));
+  };
+
+  config = mkIf (with config.services.rclone-mount; mounts != {})
+    (with config.services.rclone-mount; {
+      systemd.services = mapAttrs' mkRcloneMount mounts;
+    });
 }
