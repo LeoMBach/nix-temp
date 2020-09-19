69f15ca3e2abd4baa03be2763b0277c9328e0e4b
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-19 19:23:58 +0200

    4 -> 2 spaces

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,57 +1,57 @@
 { config, lib, pkgs, ... }:
 
 {
-    imports = [
-        ./screen.nix
-        ./tmux.nix
-        ./vim.nix
-    ];
+  imports = [
+    ./screen.nix
+    ./tmux.nix
+    ./vim.nix
+  ];
 
-    programs.vim.defaultEditor = true;
+  programs.vim.defaultEditor = true;
 
-    environment = {
-      shellAliases = {
-        ll = "ls -l";
-        lla = "ls -la";
-        llha = "ls -lha";
+  environment = {
+    shellAliases = {
+      ll = "ls -l";
+      lla = "ls -la";
+      llha = "ls -lha";
 
-        grep = "grep --color";
+      grep = "grep --color";
 
-        dir = "ls -l";
-        cls = "clear";
-      };
+      dir = "ls -l";
+      cls = "clear";
+    };
 
-      systemPackages = with pkgs; [
-        # Basic tools
-        aria2
-        coreutils
-        cron
-        cryptsetup
-        curl
-        file
-        fzf
-        git
-        htop
-        input-utils
-        less
-        lsof
-        mkpasswd
-        nmap
-        p7zip
-        pciutils
-        pigz
-        pixz
-        python38
-        ripgrep
-        rsync
-        sshfs
-        tree
-        unzip
-        usbutils
-        wget
-        zip
+    systemPackages = with pkgs; [
+      # Basic tools
+      aria2
+      coreutils
+      cron
+      cryptsetup
+      curl
+      file
+      fzf
+      git
+      htop
+      input-utils
+      less
+      lsof
+      mkpasswd
+      nmap
+      p7zip
+      pciutils
+      pigz
+      pixz
+      python38
+      ripgrep
+      rsync
+      sshfs
+      tree
+      unzip
+      usbutils
+      wget
+      zip
 
-        home-manager
+      home-manager
     ];
   };
 }
