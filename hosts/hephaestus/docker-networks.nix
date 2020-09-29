2d9acd87eec6823086d24345f8ca47b479d320a3
Leo M Bach <leo.m.bach@protonmail.com>
2020-09-29 22:33:23 +0200

    Enhance docker network init

--- hosts/hephaestus/docker-networks.nix
+++ hosts/hephaestus/docker-networks.nix
@@ -1,18 +1,26 @@
 { config, ... }:
 
 {
-  systemd.services.init-docker-networks = {
+  systemd.services.docker-init-networks = {
     description = "Create Hephaestus docker networks.";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig.type = "oneshot";
 
-    # TODO: Create multiple networks
-    # TODO: Ignore already-existing networks
-    script = let docker = "${config.virtualisation.docker.package}/bin/docker";
+    script = let
+      awk = "/run/current-system/sw/bin/awk";
+      docker = "${config.virtualisation.docker.package}/bin/docker";
     in ''
-      ${docker} network create traefik_net
+      existing_networks=$(${docker} network ls | ${awk} '{ if (NR>1) { print $2 } }' || echo true)
+      networks=("borg_net" "traefik_net")
+      for net in "''${networks[@]}"; do
+        if [[ "''${existing_networks[*]}" =~ $net ]]; then
+          echo "Docker network \"$net\" already exists."
+        else
+          ${docker} network create $net
+        fi
+      done
     '';
   };
 }
