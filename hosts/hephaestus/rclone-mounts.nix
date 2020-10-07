1d59efaa7315f4ea95c4fbc5484ce5134824c90a
Leo M Bach <leo.m.bach@protonmail.com>
2020-10-07 21:40:45 +0200

    Fix path; add missing attribute

--- hosts/hephaestus/rclone-mounts.nix
+++ hosts/hephaestus/rclone-mounts.nix
@@ -1,41 +1,43 @@
 {
-  imports = [ "../../modules/rclone-mount.nix" ];
+  imports = [ ../../modules/rclone-mount.nix ];
 
-  rclone-mount.mounts = {
-    anime = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/Anime";
-      mountPath = "/mnt/anime";
-    };
+  services = {
+    rclone-mount.mounts = {
+      anime = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/Anime";
+        mountPath = "/mnt/anime";
+      };
 
-    doc-movies = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/Documentary_Movies";
-      mountPath = "/mnt/doc-movies";
-    };
+      doc-movies = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/Documentary_Movies";
+        mountPath = "/mnt/doc-movies";
+      };
 
-    doc-tv = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/Documentary_Shows";
-      mountPath = "/mnt/doc-tv";
-    };
+      doc-tv = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/Documentary_Shows";
+        mountPath = "/mnt/doc-tv";
+      };
 
-    movies = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/Movies";
-      mountPath = "/mnt/movies";
-    };
+      movies = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/Movies";
+        mountPath = "/mnt/movies";
+      };
 
-    stand-up = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/Stand-Up";
-      mountPath = "/mnt/stand-up";
-    };
+      stand-up = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/Stand-Up";
+        mountPath = "/mnt/stand-up";
+      };
 
-    tv = {
-      configPath = "../../secrets/hephaestus/rclone.config";
-      remote = "storage-cache:Plex/TV_Shows";
-      mountPath = "/mnt/tv";
+      tv = {
+        configPath = "../../secrets/hephaestus/rclone.config";
+        remote = "storage-cache:Plex/TV_Shows";
+        mountPath = "/mnt/tv";
+      };
     };
   };
 }
