58e2efa514aa6a5b7be7f772ca4c705b0af6407b
Leo M Bach <leo.m.bach@protonmail.com>
2020-10-17 23:31:41 +0200

    Consolidate rclone mounts

--- hosts/hephaestus/rclone-mounts.nix
+++ hosts/hephaestus/rclone-mounts.nix
@@ -3,40 +3,10 @@
 
   services = {
     rclone-mount.mounts = {
-      anime = {
+      media = {
         configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/Anime";
-        mountPath = "/mnt/anime";
-      };
-
-      doc-movies = {
-        configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/Documentary_Movies";
-        mountPath = "/mnt/doc-movies";
-      };
-
-      doc-tv = {
-        configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/Documentary_Shows";
-        mountPath = "/mnt/doc-tv";
-      };
-
-      movies = {
-        configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/Movies";
-        mountPath = "/mnt/movies";
-      };
-
-      stand-up = {
-        configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/Stand-Up";
-        mountPath = "/mnt/stand-up";
-      };
-
-      tv = {
-        configPath = "${../../secrets/hephaestus/rclone.config}";
-        remote = "storage-cache:Plex/TV_Shows";
-        mountPath = "/mnt/tv";
+        remote = "storage-cache:Plex";
+        mountPath = "/mnt/media";
       };
     };
   };
