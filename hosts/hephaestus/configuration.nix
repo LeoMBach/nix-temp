c4c1aac4ff250de9c7f074fb849f9177cd070d7b
Leo M Bach <leo.m.bach@protonmail.com>
2020-11-08 10:31:43 +0100

    Remove docker-services-all systemd service

--- hosts/hephaestus/configuration.nix
+++ hosts/hephaestus/configuration.nix
@@ -24,31 +24,6 @@
 
   networking.hostName = "hephaestus";
 
-  systemd.services = {
-    docker-services-all = {
-      description = "Handles all arion-managed docker services.";
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = [''
-          /run/current-system/sw/bin/arion \
-            --pkgs ./services/arion-pkgs.nix \
-            --file ./services/portainer/arion-compose.nix \
-            up
-        ''];
-        ExecStop = [''
-          /run/current-system/sw/bin/arion \
-            --pkgs ./services/arion-pkgs.nix \
-            --file ./services/portainer/arion-compose.nix \
-            down
-        ''];
-        Restart = "always";
-        RestartSec = "10";
-      };
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-    };
-  };
-
   services.openssh.enable = true;
 
   security.sudo.wheelNeedsPassword = false;
