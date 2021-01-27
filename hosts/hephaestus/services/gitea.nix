0dc4899afba6a947d61b671182e02e00a8f38aa2
Leo M Bach <leo.m.bach@protonmail.com>
2021-01-27 21:21:15 +0100

    Use gitea wizard

--- hosts/hephaestus/services/gitea.nix
+++ hosts/hephaestus/services/gitea.nix
@@ -6,6 +6,11 @@ in
 {
   services.gitea = {
     enable = true;
+
+    appName = "Gitea";
+    disableRegistration = true;
+    useWizard = true;
+
     domain = settings.git.domain;
     rootUrl = "https://${settings.git.domain}:${builtins.toString(settings.git.port)}";
     database = {
