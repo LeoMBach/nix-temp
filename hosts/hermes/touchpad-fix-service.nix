5c86cf425faf850dc1953ef25d0c18e1e6539f26
Leo M Bach <leo.m.bach@protonmail.com>
2021-04-04 12:17:19 +0200

    Run touchpad-fix-service on resume

--- hosts/hermes/touchpad-fix-service.nix
+++ hosts/hermes/touchpad-fix-service.nix
@@ -6,15 +6,31 @@
 # TODO: Find a way to invoke this upon xorg start
 
 {
-  systemd.user.services.touchpad-fix = {
+  systemd.services.touchpad-fix = {
     description = "Re-enable touchpad";
     serviceConfig.Type = "oneshot";
 
+    environment = {
+      DISPLAY = ":0";
+      XAUTHORITY = "/home/leo/.Xauthority";
+    };
+
     path = [ pkgs.xorg.xinput ];
     script = ''
+      echo "Waiting 5 seconds..."
+      sleep 5
       touchpad="$(xinput | grep Touchpad | cut -d ' ' -f 5,6,7)"
-      echo "$touchpad"
+      echo "Found touchpad: $touchpad"
+      echo "Re-enabling touchpad..."
       xinput --enable "$touchpad"
     '';
+
+    wantedBy = [
+      "multi-user.target"
+      "sleep.target"
+      "suspend.target"
+      "hibernate.target"
+    ];
+    after = [ "sleep.target" "suspend.target" "hibernate.target" ];
   };
 }
